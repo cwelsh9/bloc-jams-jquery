5af1bba48d709ee1a019a9dbc1595d676d0aacef
camoros <caitlin.welsh9@gmail.com>
2018-01-11 21:54:17 -0700

    checkpoint 20: slider completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -40,8 +40,16 @@
     };
   });
 =======
-    
+
     player.playPause(nextSong);
   });
 >>>>>>> checkpoint-19-player-bar
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control input').val(percent);
+  }, 1000);
 }
