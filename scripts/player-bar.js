c0b4d0cfc1826cccb84dc48c16058e9d3a3458d3
camoros <caitlin.welsh9@gmail.com>
2018-01-22 15:43:16 -0700

    merging chkpt to master

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -2,7 +2,8 @@
   $('button#play-pause').on('click', function() {
     player.playPause();
   $(this).attr('playState', player.playState);
-  });
+
+});
 
   $('button#next').on('click', function() {
     if (player.playState !== 'playing') { return; }
@@ -10,7 +11,26 @@
     const nextSongIndex = currentSongIndex + 1;
     const nextSong = album.songs[nextSongIndex];
     if (nextSongIndex > album.songs.length) { return; }
-    
+
     player.playPause(nextSong);
   });
+
+  $('button#previous').on('click', function () {
+    if (player.playState == 'playing') {
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex - 1;
+      const previousSong = album.songs[previousSongIndex];
+      if (previousSongIndex < album.songs[0]) { return; }
+
+      player.playPause(previousSong);
+    };
+  });
+
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control input').val(percent);
+  }, 1000);
 }
